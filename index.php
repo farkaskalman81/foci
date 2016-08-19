99858a6167cff4c1737384a921e05b1b5c245912
farkaskalman81 <kfarkas@aiss.hu>
2016-08-19 12:55:11 +0200

    index.css hozzadva

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<title>My CSS Layout</title>
+	<link href="index.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+
+<div id="rmenu">rMenu</div>
+<div id="umenu">uMenu</div>
+<div id="content">Content</div>
+
+
+</body>
+</html>
